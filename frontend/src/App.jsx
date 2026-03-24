8a6cc18a8c5d614fc1b8c64a5c05c8fdf190688f
Yashwant sahu <sahuyashwant000@gmail.com>
2026-03-24 23:14:51 +0530

    initalize platform

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App

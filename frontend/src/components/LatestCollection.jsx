e38ad47dca3f0ac11f7dc14afbc155484e3ef163
Yashwant sahu <sahuyashwant000@gmail.com>
2026-03-26 00:34:48 +0530

    Hero section

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ShopContext } from '../context/ShopContext'
+
+const LatestCollection = () => {
+
+    const { products } = useContext(ShopContext);
+
+    console.log(products);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LatestCollection

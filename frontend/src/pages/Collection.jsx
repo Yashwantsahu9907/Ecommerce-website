89b63ae96fd1e33b60cd69a8ba836135bea4d503
Yashwant sahu <sahuyashwant000@gmail.com>
2026-03-28 08:42:25 +0530

    Add searchbar on collection page and upadtes some other files

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import Productitem from '../components/Productitem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products , search , showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -35,6 +35,11 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -74,7 +79,7 @@ const Collection = () => {
 
   useEffect(() =>{
     applyFilter()
-}, [category, subCategory])
+}, [category, subCategory, search, showSearch])
   
 
 useEffect (()=> {

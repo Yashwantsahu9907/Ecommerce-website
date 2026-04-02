cdb6b67eac79a8824423a0b124b328fcb6af2d28
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-02 15:09:59 +0530

    Payment method

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState, } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 
  export const ShopContext = createContext();
@@ -12,6 +13,7 @@ import { toast } from "react-toastify";
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
+    const navigate = useNavigate();
     
 
 
@@ -92,7 +94,7 @@ import { toast } from "react-toastify";
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount
+        getCartAmount, navigate
     }
     return (
         <ShopContext.Provider value={value}>

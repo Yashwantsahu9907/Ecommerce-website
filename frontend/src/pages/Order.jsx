295849839896926194b61dbf15138b3e959a5fcf
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-06 23:15:24 +0530

    Add track option

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -35,6 +35,7 @@ const Order = () => {
                   <p className='min-w-2 h-2 rounded-full bg-green-600'></p>
                   <p className='text-sm md:text-base'>Ready to ship </p>
                 </div>
+                <button className='border border-gray-200 px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
               </div>
             </div>
           ))

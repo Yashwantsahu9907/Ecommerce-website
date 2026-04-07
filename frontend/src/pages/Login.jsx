18ff8b7adfe4b9029127f0318ec264d58ae35827
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-07 22:55:27 +0530

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,7 +10,9 @@ const Login = () => {
         <p className='prate-regular text-3xl'>{currentState}</p>
         <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
       </div>
-
+      {currentState !== 'Login' && (<input type="text"className="w-full px-3 py-2 border border-gray-800"placeholder="Name"required />)}
+      <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='Email' required />
+      <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Password' />
     </form>
   )
 }

bc8cddda397070ae0484af2186227206335e9d1c
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-08 08:35:59 +0530

    About section

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+
 
 const About = () => {
   return (
     <div>
-      
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col  justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Forever was born out of a passion for innovation and a desire to revolution  the way people shop onlime. Our journey began with a simple idea: to provide a platform where customers can easily discover, explore, and purchase a wide range of products from the  comfort of their homes</p>
+          <p>Since our inception, we've worked tirelessely to curate a diverse  selection of high-quality  products that cater to every taste and preference . From fashion and beauty to electronics and home essential, we offer an extensive collection sourced from trusted brands and suppliers.</p>
+        </div>
+      </div>
+
     </div>
   )
 }

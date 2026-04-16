533a0ec560bd9ef2af1a0205980afdf6d5597031
Yashwant sahu <sahuyashwant000@gmail.com>
2026-04-16 09:26:00 +0530

    controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,63 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+import userModel from "../models/userModel.js";
+
+
+
+const createToken = (id) => {
+return jwt.sign({id}, process.env.JWT_SECRET)
+}
 
 //------------Route for user login----------------------------
 
 const loginUser = async (req, res) => {
-
+   
 }
 
 //-------------Route for user register--------------------
 const registerUser = async (req, res) => {
 
-    res.json({msg:"Register API Working"})
+    try {
+        const { name, email, password } = req.body;
+
+        // checking user already exist or not 
+        const exist = await userModel.findOne({ email });
+        if (exist) {
+            return res.json({ success: false, message: "User already exist " })
+        }
+
+        // vailidating  email and strong password
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email " })
+        }
+
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Password must be at least 8 characters long " })
+        }
+
+        //hashing user password
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
+            name,
+            email,
+            password:hashedPassword
+        })
+
+        const user = await newUser.save() 
+        const token = createToken(user._id)
+
+        res.json({success:true,token})
+
+    } catch (error) {
+console.log(error);
+res.json({success:false, message:error.message})
+    }
 }
+
 
 
 
@@ -18,6 +66,6 @@ const registerUser = async (req, res) => {
 const adminLogin = async (req, res) => {
 
 }
- 
+
 
 export { loginUser, registerUser, adminLogin }
